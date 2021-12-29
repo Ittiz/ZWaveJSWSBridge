1e464db055cef539fc7850956bad70d5d5a59c1c
Ittiz <github@daein.org>
2021-12-28 19:11:42 -0500

    Simple two way communication now works.  Bug fixes and typos.

--- README.md
+++ README.md
@@ -4,24 +4,53 @@ This project is to create a bridge for ZWaveJS2MQTT to Domoticz, for updates Dom
 This project uses the Websockets server of ZWaveJS2MQTT to monitor updates from ZWaveJS2MQTT and transfer them to
 devices in Domoticz.  The devices are set up in the devices.ini file and the plugin creates them when it receives
 the request.  This is useful for cases where you have updates that domoticz doesn't understand by turning them
-into a domoticz device through the ini file. See the devices.ini file for instructions on use. You can take 
-something like Thermostat State, which is not handled in Domoticz, and turn it into a device which tells you that
-your furnace is off or on.
+into a domoticz device through the ini file. You can take  something like Thermostat State, which is not handled
+in Domoticz, and turn it into a device which tells you that your furnace is off or on.
 
 Why'd I use Websocket instead of MQTT?  Two reasons: Websockets don't need a mosquitto server and I have more
 experience with web sockets.
 
 # Installation
-If you don't know how already, wait for this plugin to be more developed!
+Goto your plugin folder and clone this repo.
+
+# How to set up new devices
+The devices.ini file defines the Zwave devices you want to creat and how youy interact with them. This is an
+example device:
+
+[2.0]                           ;Device node ID in ZWaveJS2MQTT the period followed by a number is for adding more than one Domoticz device per ZWaveJS2MQTT node
+name=2nd Floor Fire Alarm State ;Name of the domoticz device to change.      /|If two Domoticz devices have the same name the index should be same too.
+index=1                         ;Plugin hardware ID #. You decide what it is.\|Example: Using two ZWaveJS2MQTT updates to change the s and n values of 1 device.
+typeID=243                      ;The device type ID #. See Domoticz device types: https://www.domoticz.com/wiki/Developing_a_Python_plugin#Available_Device_Types
+subTypeID=31                    ;The subtype # ID of device.  See Domoticz device subtypes: https://www.domoticz.com/wiki/Developing_a_Python_plugin#Available_Device_Types
+switchTypeID=0                  ;Switch type ID #.  See Domoticz device switchtypes: https://www.domoticz.com/wiki/Developing_a_Python_plugin#Available_Device_Types
+commandClass=Notification       ;What Zwave command class this device is.(Str/Int)<--ALWAYS NEEDED
+endpoint=0                      ;When there are multple properties of the same name. 0 if not needed. (Int)<--Not always needed
+property=alarmType              ;Property name of event in ZWaveJS2MQTT to change in Domoticz.(Str/Int)<--ALWAYS NEEDED
+propertyKey=0                   ;When there are multple properties of the same name. 0 if not needed. (Int)<--Not always needed
+value=sValue                    ;What property of the device in Domoticz we want to change
+image=0                         ;Image you'd like for the device. Differs for each set up. Check http://127.0.0.1/json.htm?type=custom_light_icons on your server.
+options=                        ;Python Dictionary contaning options. Depends on your device. Advanced stuff look up Domoticz documentation.
+description=                    ;self explanatory.
+tempInput=                      ;Temperature devices need input units. Domoticz ALWAYS stores values as Celcius. Use: C, F, K and Blank for not temp device.
+direction=out                   ;Are we getting putting data? For in & out on one device make 2 sections with the same index.
+enabled=1                       ;set to zero for the plugin to ignore this device.
+
+You can findou which CommandClass, endpoint, property and propertyKey values you need from the ZWaveJS2MQTT
+control panel usually found at http://127.0.0.1:8091/control-panel on your server.
+
+![This is how you tell the plugin which device/property you want to target](/ZWJS2MQTTWB.png)
 
 # TODO
 1: I'd like to add a way to translate inputs to different outputs using the ini file.  So a device changing its
    status to 1 equals a different input to the Domoticz device like to "Danny" for instance when you get a number
    in an update that represents a particular user who has logged into an external hardware.
+
 2: Create a way to add and edit devices from the web UI.
+
 3: Any other ideas people have to make this plugin more useful!  It could replace MQTT all together given enough
    input.
-4: Communication is currently ONE way! From the device to the server only.  Out bound needs to be added as well.
+
+4: More kinds of input devices.
 
 # Lastly
 This plugin is in early developement.  If it doesn't work come back later. Also provide feed back if it
--- plugin.py
+++ plugin.py
@@ -3,7 +3,7 @@
 # Author: Ittiz
 #
 """
-<plugin key="ZWaveJS2MQTTBridge" name="ZWaveJS2MQTT Websocket Bridge Plugin" author="Ittiz" version="0.1.0" externallink="https://github.com/Ittiz/ZWaveJSWSBridge">
+<plugin key="ZWaveJS2MQTTBridge" name="ZWaveJS2MQTT Websocket Bridge Plugin" author="Ittiz" version="0.1.1" externallink="https://github.com/Ittiz/ZWaveJSWSBridge">
     <description>
         <h2>ZWaveJS2MQTT Websocket Bridge Plugin</h2><br/>
         Websocket bridge between ZWaveJS2MQTT and Domoticz.
@@ -36,6 +36,24 @@ import queue
 config = configparser.ConfigParser()
 
 debug = False
+
+def fixtemp(direction,tempInput,newValue):#we need this in multiple places so it might as well be a function.
+    if direction == "in":
+        if str(tempInput) == "F" or str(tempInput) == "f":
+            newValue = float(newValue)*(9/5)+32
+        elif str(tempInput) == "K" or str(tempInput) == "k":
+            newValue = float(newValue)+273.15
+        else:
+            newValue = float(newValue)
+    else:
+        if str(tempInput) == "F" or str(tempInput) == "f":
+            newValue = (float(newValue)-32)*(5/9)
+        elif str(tempInput) == "K" or str(tempInput) == "k":
+            newValue = float(newValue)-273.15
+        else:
+            newValue = float(newValue)
+    return newValue
+
 # The main function that will handle connection and communication 
 # with the server
 async def listen():
@@ -55,40 +73,63 @@ async def listen():
             await asyncio.wait_for(ws.send('{"messageId": "start-listening-result", "command": "start_listening"}'),1)#This tells ZwaveJS2MQTT Websocket server we want updates about Zwave device status changes.
             msg = await asyncio.wait_for(ws.recv(),1)#This is the initial state of all the nodes, lets ignore this for now.  TODO:  Check all the states for our devices and fix incorrect things!
             #Domoticz.Log(msg)
-            config.read("plugins/ZWaveJSWSBridge/devices.ini")
+            try:
+                config.read("plugins/ZWaveJSWSBridge/devices.ini")#try and get our device info from the ini file.
+            except Exception as err:
+                Domoticz.Error(str(err))
             while True:
-                try:
+                try:#This is for inputting messages back to ZWaveJS2MQTT and also alerting the script the loop and thread need to stop.
                     #Domoticz.Log(str(WebSocketInput.empty()))
                     if WebSocketInput.empty() is False:
                         WSInput = WebSocketInput.get(block=True)
-                        if WSInput is None:
+                        if WSInput is None:#kill signal has been issued!
                             if debug:
                                 Domoticz.Log("Exiting input handler")
-                            WebSocketInput.task_done()
+                            WebSocketInput.task_done()#tell the Webocket server we're done here.
                             loop.stop()#if we're in here the thread is going away so we add a precautionary stop of loop
-                            break#Keep in mind that if the loop never gets here the thread never stops.
-                        WebSocketInput.task_done()
+                            break#Keep in mind that if the loop never gets here the thread never stops. Kills the thread!
+                        else:
+                            try:
+                                await asyncio.wait_for(ws.send(WSInput),1)#This tells ZwaveJS2MQTT Websocket server we want to update a device.
+                                msg = await asyncio.wait_for(ws.recv(),1)#check to see if ZwaveJS2MQTT has a reply.
+                                if debug:
+                                    Domoticz.Log(msg)
+                            except Exception as err:
+                                    Domoticz.Error(str(err))
+                        WebSocketInput.task_done()#tell the Webocket server we're done here.
                 except:
                     if debug:
                         Domoticz.Log("No input for websocket in queue")
-                try:
-                    #Domoticz.Log("out")
+                try:#This is where we get updates from ZwaveJS2MQTT and send them to our devices.
                     msg = await asyncio.wait_for(ws.recv(),1)#ALWAYS put a timeout for "wait_for(..." or a dormant connection will cause the plugin to hang!
-                    data = json.loads(msg)
+                    data = json.loads(str(msg))
                     if debug:
                         Domoticz.Log("Update:"+str(msg))
                     node = data['event']['nodeId']
                     try:
-                        commandclass = data['event']['args']['commandClassName']
+                        commandclassName = data['event']['args']['commandClassName']
+                        commandclass = data['event']['args']['commandClass']
                         propertyname = data['event']['args']['propertyName']
                         newValue = data['event']['args']['newValue']
+                        try:
+                            endpoint = data['event']['args']['endpoint']
+                        except:
+                            endpoint = 0
+                        try:
+                            propertykey = data['event']['args']['propertyKey']
+                        except:
+                            propertykey = 0
+                        try:
+                            isinstance(int(config.get(nodeId, "commandClass")),int)#hackish way to see if the user input the command class number rather than name
+                        except:
+                            commandclass = commandclassName#json returned in update seperates the command class name and number but we can use either so lets use the one in the ini.
                     except:
                         #Domoticz.Log("fail!")
                         commandclass = None
                         propertyname = None
                         newValue = None
                     if debug:
-                        Domoticz.Log("Node:"+str(node)+" Command Class:"+str(commandclass)+", "+" Property Name:"+str(propertyname))
+                        Domoticz.Log("Node:"+str(node)+" Command Class:"+str(commandclass)+", Endpoint:"+str(endpoint)+", Property Name:"+str(propertyname)+", Property Key:"+str(propertykey))
                     sectionsfound = 1
                     i = 0
                     while sectionsfound == 1 and commandclass != None and propertyname != None and newValue != None:
@@ -100,46 +141,57 @@ async def listen():
                             pIndex = int(config.get(nodeId, "index"))
                             DeviceID="ZW2WSB-"+str(node)+"."+str(pIndex)
                             tempInput = str(config.get(nodeId, "tempInput"))
-                        except:
+                            direction = str(config.get(nodeId, "direction"))
+                            #Domoticz.Log("Info:"+str(Devices[DeviceID]))
+                            if debug:
+                                Domoticz.Log("Info:"+str(pIndex)+", "+commandclass+", "+config.get(nodeId, "commandClass")+", "+str(endpoint)+", "+config.get(nodeId, "endpoint")+", "+propertyname+", "+config.get(nodeId, "property")+", "+str(propertykey)+", "+config.get(nodeId, "propertyKey"))
+                            if direction == "in":#This is for creating input devices.  We seperate the inputs from the outputs because we may want to send our commands to a seperate property or even command class than we recieved updates from.
+                                try:
+                                    Devices[DeviceID]
+                                except:
+                                    if enabled == 1:
+                                        try:
+                                            Domoticz.Log("Creating new iput device: "+str(config.get(nodeId, "name")))
+                                            device = Domoticz.Unit(Name=str(config.get(nodeId, "name")), DeviceID=DeviceID, Unit=int(pIndex), Type=int(config.get(nodeId, "typeID")), Subtype=int(config.get(nodeId, "subTypeID")), Switchtype=int(config.get(nodeId, "switchTypeID")), Image=int(config.get(nodeId, "image")), Options=str(config.get(nodeId, "options")), Used=1, Description=str(config.get(nodeId, "description"))).Create()
+                                        except Exception as err:
+                                            Domoticz.Error("Failed to create device! Error: "+str(err)+" node "+nodeId+" will be ignored")
+                                            sectionsfound = 0
+                            else:
+                                if enabled == 1 and commandclass == config.get(nodeId, "commandClass") and endpoint == int(config.get(nodeId, "endpoint")) and propertyname == config.get(nodeId, "property") and propertykey == int(config.get(nodeId, "propertyKey")):#if it's not enabled we don't need to do anything.
+                                    if debug:
+                                        Domoticz.Log(str("Update property found in config!"))
+                                    try:
+                                        keyType = config.get(nodeId, "value")
+                                        try:
+                                            device = Devices[DeviceID].Units[pIndex]
+                                            if len(tempInput) > 0:
+                                                newValue = fixtemp(direction,tempInput,newValue)
+                                            if (keyType == "nValue"):
+                                                Domoticz.Log("Update: "+str(config.get(nodeId, "name"))+", nValue: "+str(newValue))
+                                                device.nValue=int(newValue)
+                                            elif (keyType == "sValue"):
+                                                Domoticz.Log("Update: "+str(config.get(nodeId, "name"))+", sValue: "+str(newValue))
+                                                device.sValue=str(newValue)
+                                            device.Update(Log=True)
+                                        except Exception as err:
+                                            Domoticz.Error(str(err))
+                                            try:
+                                                Domoticz.Log("Failed to update device! Is it new? Creating: "+str(config.get(nodeId, "name")))
+                                                i=i-1#we do this to catch the correct value from this update on creation
+                                                device = Domoticz.Unit(Name=str(config.get(nodeId, "name")), DeviceID=DeviceID, Unit=int(pIndex), Type=int(config.get(nodeId, "typeID")), Subtype=int(config.get(nodeId, "subTypeID")), Switchtype=int(config.get(nodeId, "switchTypeID")), Image=int(config.get(nodeId, "image")), Options=str(config.get(nodeId, "options")), Used=1, Description=str(config.get(nodeId, "description"))).Create()
+                                            except Exception as err2:
+                                                Domoticz.Error("Failed to create device! Error: "+str(err)+" and "+str(err2)+" node "+nodeId+" will be ignored")
+                                                sectionsfound = 0
+                                    except:
+                                        if debug:
+                                            Domoticz.Log("Node "+nodeId+" not in Config")
+                        except Exception as err:
+                            if debug:
+                                Domoticz.Error(str(err))
                             sectionsfound = 0
                             enabled=0
                             commandclass = 0
                             propertyname = 0
-                        if debug:
-                            Domoticz.Log("Info:"+str(pIndex)+", "+commandclass+", "+config.get(nodeId, "commandClass")+", "+propertyname+", "+config.get(nodeId, "property"))
-                        if enabled == 1 and commandclass == config.get(nodeId, "commandClass") and propertyname == config.get(nodeId, "property"):#if it's not enabled we don't need to do anything.
-                            if debug:
-                                Domoticz.Log(str("Update property found in config!"))
-                            try:
-                                keyType = config.get(nodeId, "value")
-                                try:
-                                    device = Devices[DeviceID].Units[pIndex]
-                                    if len(tempInput) > 0:
-                                        if str(tempInput) == "F" or str(tempInput) == "f":
-                                            newValue = (float(newValue)-32)*(5/9)
-                                        elif str(tempInput) == "K" or str(tempInput) == "k":
-                                            newValue = float(newValue)-273.15
-                                        else:
-                                            newValue = float(newValue)
-                                    if (keyType == "nValue"):
-                                        Domoticz.Log("Update: "+str(config.get(nodeId, "name"))+", nValue: "+str(newValue))
-                                        device.nValue=int(newValue)
-                                    elif (keyType == "sValue"):
-                                        Domoticz.Log("Update: "+str(config.get(nodeId, "name"))+", sValue: "+str(newValue))
-                                        device.sValue=str(newValue)
-                                    device.Update(Log=True)
-                                except Exception as err:
-                                    Domoticz.Error(str(err))
-                                    try:
-                                        Domoticz.Log("Failed to update device! Is it new? Creating: "+str(config.get(nodeId, "name")))
-                                        i=i-1#we do this to catch the correct value from this update on creation
-                                        device = Domoticz.Unit(Name=str(config.get(nodeId, "name")), DeviceID=DeviceID, Unit=int(pIndex), Type=int(config.get(nodeId, "typeID")), Subtype=int(config.get(nodeId, "subTypeID")), Switchtype=int(config.get(nodeId, "switchTypeID")), Image=int(config.get(nodeId, "image")), Options=str(config.get(nodeId, "options")), Used=1, Description=str(config.get(nodeId, "description"))).Create()
-                                    except Exception as err2:
-                                        Domoticz.Error("Failed to create device! Error: "+str(err)+" and "+str(err2)+" node "+nodeId+" will be ignored")
-                                        sectionsfound = 0
-                            except:
-                                if debug:
-                                    Domoticz.Log("Node "+nodeId+" not in Config")
                         i=i+1
                 except Exception as err:
                     if debug and len(str(err)) > 0:
@@ -204,7 +256,65 @@ class BasePlugin:
         Domoticz.Log("onMessage called")
 
     def onCommand(self, Unit, Command, Level, Hue, Whatever):
-        Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level) + ", Hue: " + str(Hue) + ", Whatever: " + str(Whatever))#Domoticz.Log("onCommand called")#
+        #Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Command '" + str(Command) + "', Level: " + str(Level) + ", Hue: " + str(Hue) + ", Whatever: " + str(Whatever))#Domoticz.Log("onCommand called")#
+        node = Unit[7:Unit.find(".",7)]
+        try:
+            config.read("plugins/ZWaveJSWSBridge/devices.ini")#try and get our device info from the ini file.
+        except Exception as err:
+            Domoticz.Error(str(err))
+        #WebSocketInput.put('{"messageId": "Test_input" ,"command": "node.set_value", "nodeId": 19, "valueId": {"commandClass": 37, "property": "targetValue"}, "value":false}')
+        sectionsfound = 1
+        i = 0
+        while sectionsfound == 1:
+            if debug:
+                Domoticz.Log(str("Input node found in config!"))
+            try:
+                nodeId = str(node)+"."+str(i)
+                enabled = int(config.get(nodeId, "enabled"))
+                pIndex = int(config.get(nodeId, "index"))
+                tempInput = str(config.get(nodeId, "tempInput"))
+                direction = str(config.get(nodeId, "direction"))
+                commandClass = str(config.get(nodeId, "commandClass"))
+                prop = str(config.get(nodeId, "property"))
+                typeId = int(config.get(nodeId, "typeID"))
+                #Domoticz.Log("Info:"+str(Devices[DeviceID]))
+                if direction == "in" and enabled == 1 and str(Command) == str(pIndex):
+                    try:
+                        endpoint = config.get(nodeId, "endpoint")
+                    except:
+                        endpoint = None
+                    try:
+                        propertykey = config.get(nodeId, "propertyKey")
+                    except:
+                        propertykey = None
+                    message = '{"messageId": "'+str(Unit)+'_input" ,"command": "node.set_value", "nodeId": '+str(node)+', "valueId": {"commandClass": '
+                    try:
+                        isinstance(int(commandClass),int)#hackish way to see if the user input the command class number rather than name
+                        message = message+str(commandClass)#if a number is input we don't need the quotes in the json.
+                    except:
+                        message = message+'"'+str(commandClass)+'"'
+                    if endpoint != None:
+                        message = message + ', "endpoint": '+str(endpoint)
+                    message =  message + ', "property": "'+str(prop)+'"'
+                    if propertykey != None:
+                        message = message + ', "propertyKey": '+str(propertykey)
+                    if typeId == 242:#thermostat
+                        Hue = int(round(float(Hue)))#Domoticz can do half degrees, some thermostats don't like this.
+                        message = message+'}, "value":'+str(Hue)+'}'
+                    else:
+                        message = message+'}, "value":'+str(Hue)+'}'
+                    if debug:
+                        Domoticz.Log("Message Input: "+message)
+                    Domoticz.Log("Updating "+str(Unit)+" to "+str(Hue))
+                    WebSocketInput.put(message)#send our device update to our websocket thread handle.
+            except Exception as err:
+                if debug:
+                    Domoticz.Error(str(err))
+                sectionsfound = 0
+                enabled=0
+                commandclass = 0
+                propertyname = 0
+            i=i+1
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
         Domoticz.Log("Notification Called")#Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
@@ -213,6 +323,11 @@ class BasePlugin:
         Domoticz.Log("onDisconnect called")
 
     def onHeartbeat(self):
+        #try:
+        #    config.read("plugins/ZWaveJSWSBridge/devices.ini")
+        #    isinstance(int(config.get("6.1", "commandClass")),int)
+        #except Exception as err:
+        #    Domoticz.Error(str(err))
         #Domoticz.Log(str(loop.running())+" is running.")
         #if loop.is_running() == False and self.notStopping: #Something happened. Attempt a restart!
         #    Domoticz.Log("Something disconnected the Websocket.  Attempting to reconnect!")
